143e6c959ad60c3ad743ff3d378fdd4238d62d3f
Raj Katipally <rkatipally@atpco.net>
2019-06-14 17:42:52 -0400

    refactored check_permutation

--- src/main/java/org/raj/cci/c1/CheckPermutation.java
+++ src/main/java/org/raj/cci/c1/CheckPermutation.java
@@ -5,6 +5,7 @@ import java.util.HashMap;
 public class CheckPermutation {
 
     public static boolean checkPermutation(String str1, String str2){
+        if(str1.length() != str2.length()) return false;
         HashMap<Character, Integer> charCountMap = new HashMap<>();
         for(int i=0; i<str1.length(); i++){
             int count = charCountMap.get(str1.charAt(i)) == null? 0 : charCountMap.get(str1.charAt(i));
@@ -13,11 +14,9 @@ public class CheckPermutation {
 
         for (int i=0; i < str2.length() ; i++){
             int count = charCountMap.get(str2.charAt(i));
-            count = count-1;
+            charCountMap.put(str2.charAt(i), count-1);
             if(count < 0) return false;
-            charCountMap.put(str2.charAt(i), count);
-            if(count == 0) charCountMap.remove(str2.charAt(i));
         }
-        return charCountMap.size() == 0;
+        return true;
     }
 }
